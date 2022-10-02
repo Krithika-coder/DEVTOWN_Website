aa04a8a5216d5fda3850205eb7e2c79104bd38e1
Krithika <85865229+Krithika-coder@users.noreply.github.com>
2022-10-02 18:55:37 +0530

    Add files via upload

---
+++ Components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp</h1>
+      <p>
+        All the students who have 100% attendance and complete the project will
+        recieve a certificate from DevTown!
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
